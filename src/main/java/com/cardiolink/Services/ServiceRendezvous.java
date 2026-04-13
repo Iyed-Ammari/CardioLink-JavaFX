786ede7d92b41a78d0053e52cd51f537018eb367
chaima125 <chaymakamel3@gmail.com>
2026-04-13 20:05:00 +0100

    Ajout entités rendez-vous et ordonnance avec CRUD

---
+++ src/main/java/com/cardiolink/Services/ServiceRendezvous.java
@@ -0,0 +1,108 @@
+package com.cardiolink.Services;
+
+import com.cardiolink.Models.Rendezvous;
+import com.cardiolink.utils.MyDatabase;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServiceRendezvous implements Iservice<Rendezvous> {
+
+    private Connection connection;
+
+    public ServiceRendezvous() {
+        connection = MyDatabase.getInstance().getConnection();
+    }
+
+    @Override
+    public void add(Rendezvous rv) throws SQLDataException {
+        // Changement du nom de la table ici
+        String query = "INSERT INTO rendez_vous (date_heure, statut, type, lien_visio, remarques, patient_id, medecin_id) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        try (PreparedStatement pst = connection.prepareStatement(query)) {
+            pst.setTimestamp(1, Timestamp.valueOf(rv.getDateHeure()));
+            pst.setString(2, rv.getStatut());
+            pst.setString(3, rv.getType());
+            pst.setString(4, rv.getLienVisio());
+            pst.setString(5, rv.getRemarques());
+            pst.setInt(6, rv.getPatientId());
+            pst.setInt(7, rv.getMedecinId());
+            pst.executeUpdate();
+            System.out.println("Rendez-vous ajouté !");
+        } catch (SQLException e) {
+            System.err.println("Erreur add rdv: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public List<Rendezvous> getAll() throws SQLDataException {
+        List<Rendezvous> liste = new ArrayList<>();
+        String query = "SELECT * FROM rendez_vous";
+        try (Statement st = connection.createStatement();
+             ResultSet rs = st.executeQuery(query)) {
+            while (rs.next()) {
+                Rendezvous rv = new Rendezvous();
+                rv.setId(rs.getInt("id"));
+                Timestamp ts = rs.getTimestamp("date_heure");
+                if (ts != null) rv.setDateHeure(ts.toLocalDateTime());
+                rv.setStatut(rs.getString("statut"));
+                rv.setType(rs.getString("type"));
+                rv.setPatientId(rs.getInt("patient_id"));
+                rv.setMedecinId(rs.getInt("medecin_id"));
+                liste.add(rv);
+            }
+        } catch (SQLException e) {
+            System.err.println("Erreur getAll rdv: " + e.getMessage());
+        }
+        return liste;
+    }
+
+    @Override
+    public void update(Rendezvous rv) throws SQLDataException {
+        String query = "UPDATE rendez_vous SET date_heure=?, statut=?, type=?, remarques=? WHERE id=?";
+        try (PreparedStatement pst = connection.prepareStatement(query)) {
+            pst.setTimestamp(1, Timestamp.valueOf(rv.getDateHeure()));
+            pst.setString(2, rv.getStatut());
+            pst.setString(3, rv.getType());
+            pst.setString(4, rv.getRemarques());
+            pst.setInt(5, rv.getId());
+            pst.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println("Erreur update rdv: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void delete(Rendezvous rv) throws SQLDataException {
+        String query = "DELETE FROM rendez_vous WHERE id = ?";
+        try (PreparedStatement pst = connection.prepareStatement(query)) {
+            pst.setInt(1, rv.getId());
+            pst.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println("Erreur delete rdv: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public Rendezvous getById(int id) throws SQLDataException {
+        String query = "SELECT * FROM rendez_vous WHERE id = ?";
+        try (PreparedStatement pst = connection.prepareStatement(query)) {
+            pst.setInt(1, id);
+            ResultSet rs = pst.executeQuery();
+            if (rs.next()) {
+                Rendezvous rv = new Rendezvous();
+                rv.setId(rs.getInt("id"));
+                Timestamp ts = rs.getTimestamp("date_heure");
+                if (ts != null) rv.setDateHeure(ts.toLocalDateTime());
+                rv.setStatut(rs.getString("statut"));
+                rv.setType(rs.getString("type"));
+                rv.setPatientId(rs.getInt("patient_id"));
+                rv.setMedecinId(rs.getInt("medecin_id"));
+                return rv;
+            }
+        } catch (SQLException e) {
+            System.err.println("Erreur getById rdv: " + e.getMessage());
+        }
+        return null;
+    }
+}
