4e1986e3f42ac27315fd54dad01046d72c386c05
Iyed-Ammari <ammariiyed@isamm.u-manouba.tn>
2026-04-16 03:39:53 +0100

    refactor

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -21,16 +21,16 @@ module com.cardiolink {
 
     // Package Exports
     exports com.cardiolink;
-    exports com.cardiolink.Controllers;
     exports com.cardiolink.Models;
     exports com.cardiolink.Services;
     exports com.cardiolink.Test;
 
     // Reflection Opens (Required for JavaFX FXML and Property access)
     opens com.cardiolink to javafx.fxml;
-    opens com.cardiolink.Controllers to javafx.fxml;
     opens com.cardiolink.Models to javafx.fxml, javafx.base;
     opens com.cardiolink.Services to javafx.fxml;
     opens com.cardiolink.utils to javafx.fxml;
     opens com.cardiolink.Test to javafx.fxml;
+    exports com.cardiolink.Controllers;
+    opens com.cardiolink.Controllers to javafx.fxml;
 }
