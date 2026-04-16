dc81c46d47ae6f133ebf920afe30765a17d3f9ff
Iyed-Ammari <ammariiyed@isamm.u-manouba.tn>
2026-04-16 14:33:36 +0100

    refactor the Test/main to avoid conflicts

--- src/main/java/com/cardiolink/Test/Main.java
+++ src/main/java/com/cardiolink/Test/Main.java
@@ -11,23 +11,6 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
 
-        try {
-
-            Parent root = FXMLLoader.load(
-                    getClass().getResource("/post_view.fxml")
-            );
-
-            Scene scene = new Scene(root);
-
-            stage.setTitle("CardioLink - Forum");
-            stage.setScene(scene);
-            stage.setWidth(1200);
-            stage.setHeight(800);
-            stage.show();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     public static void main(String[] args) {
